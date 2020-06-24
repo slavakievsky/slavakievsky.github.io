e3cf778a42384720446120b8109f206d6fc20bb9
pushsla <push2001sla@gmail.com>
2020-06-24 15:40:13 +0500

    (Internal) Yandex metrika

---
+++ _includes/js-yandexmetrica.js
@@ -0,0 +1,14 @@
+<!-- START Yandex.Metrika -->
+<script>
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(57205813, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript><div class="nscrpt"><img src="https://mc.yandex.ru/watch/57205813" style="position:absolute; left:-9999px;" alt="yandex"/></div></noscript>
+<!-- END Yandex.Metrika -->
