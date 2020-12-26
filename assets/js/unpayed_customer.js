898209f0e88753ae2e9bb7cf24fcd6a8c1bc7c5e
slavakievsky <slavakievsky@gmail.com>
2020-12-26 13:11:21 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -5,7 +5,7 @@ const unpayers = {
 let host = window.location.hostname;
 let now = Date.now();
 
-if (unpayers.hasOwnProperty(host)){
+if (unpayers.hasOwnProperty(host) && now > unpayers[host]){
   if (now - unpayers[host] > 2592000000){
     alert("Владелец этого ресурса задержал оплату работы более, чем на месяц =( Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com");
   }else{
