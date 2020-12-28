fd86174446ce9ae27b016aa26450670b4c89f564
slavakievsky <slavakievsky@gmail.com>
2020-12-28 11:05:09 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -20,7 +20,7 @@ if (unpayed_unpayers.hasOwnProperty(unpayed_host) && unpayed_now > unpayed_unpay
   if (unpayed_now - unpayed_unpayers[unpayed_host] > 25920000000){
     alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
     unpayed_banner.innerHTML = "Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com";
-    unpayed_banner_place.prepend(unpayed_banner);
+    unpayed_banner_place.append(unpayed_banner);
   }else if (unpayed_now - unpayed_unpayers[unpayed_host] > 2592000000){
     alert("Владелец этого ресурса не оплатил работы по разработке сайта. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
   }else{
