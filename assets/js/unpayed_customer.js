3c1f88144b4c4f990db75e890c9769a7306a299e
slavakievsky <slavakievsky@gmail.com>
2020-12-28 10:28:59 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -1,16 +1,20 @@
 const unpayed_unpayers = {
-  "ola.lsport.net": new Date('2021-01-25')
+  "ola.lsport.net": new Date('2021-01-25'),
+  "slavakievsky.github.io": new Date('2020-12-20')
 };
 
 let unpayed_host = window.location.hostname;
 let unpayed_now = Date.now();
+let unpayed_banner_place = document.head;
+let unpayed_banner = document.createElement("h1");
 
 if (unpayed_unpayers.hasOwnProperty(unpayed_host) && unpayed_now > unpayed_unpayers[unpayed_host]){
   if (unpayed_now - unpayed_unpayers[unpayed_host] > 25920000000){
     alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
   }else if (unpayed_now - unpayed_unpayers[unpayed_host] > 2592000000){
-    alert("Владелец этого ресурса не оплатил мою работу. Уже и месяц пролетел с тех пор =( Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
+    alert("Владелец этого ресурса не оплатил работы по разработке сайта. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
   }else{
-    alert('Владелец этого ресурса не оплатил работу. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
+    unpayed_banner.innerHTML = 'Исполнитель ждёт оплаты своей работы :D Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com';
+    unpayed_banner_place.prepend(unpayed_banner)
   }
 }
