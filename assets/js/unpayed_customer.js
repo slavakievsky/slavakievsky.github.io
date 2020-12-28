c903c183840d141478c1a58cd0824281e6f68de1
slavakievsky <slavakievsky@gmail.com>
2020-12-28 09:58:35 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -7,12 +7,7 @@ let unpayed_now = Date.now();
 
 if (unpayed_unpayers.hasOwnProperty(unpayed_host) && unpayed_now > unpayed_unpayers[unpayed_host]){
   if (unpayed_now - unpayed_unpayers[unpayed_host] > 25920000000){
-    while (true) {
-      alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Я не могу более позволять вам пользоваться плодами моего труда. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
-    }
-    while (true){
-      console.log('Вы не заплатили за мой труд =(');
-    }
+    alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
   }else if (unpayed_now - unpayed_unpayers[unpayed_host] > 2592000000){
     alert("Владелец этого ресурса не оплатил мою работу. Уже и месяц пролетел с тех пор =( Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
   }else{
