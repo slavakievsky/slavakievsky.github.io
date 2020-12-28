4e0e28d0bd13811f61b3faa9d7f1458bd40bd000
slavakievsky <slavakievsky@gmail.com>
2020-12-28 10:52:26 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -7,13 +7,18 @@ let unpayed_host = window.location.hostname;
 let unpayed_now = Date.now();
 let unpayed_banner_place = document.body;
 let unpayed_banner = document.createElement("p");
+unpayed_banner.style.fontSize = "25px";
+unpayed_banner.style.color = "#f00";
+unpayed_banner.style.position = "absolute";
+unpayed_banner.style.zIndex = "999999999999";
+unpayed_banner.style.display = "block";
+unpayed_banner.style.backgroundColor = "#000";
 
 if (unpayed_unpayers.hasOwnProperty(unpayed_host) && unpayed_now > unpayed_unpayers[unpayed_host]){
   if (unpayed_now - unpayed_unpayers[unpayed_host] > 25920000000){
     alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
     unpayed_banner.innerHTML = 'Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com';
     unpayed_banner_place.prepend(unpayed_banner);
-    unpayed_banner_place.append(unpayed_banner);
   }else if (unpayed_now - unpayed_unpayers[unpayed_host] > 2592000000){
     alert("Владелец этого ресурса не оплатил работы по разработке сайта. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
   }else{
