d962e4196f0ccb8157f155d76f79dc4276cd7593
slavakievsky <slavakievsky@gmail.com>
2020-12-28 11:03:08 +0300

    unpayed customers

--- assets/js/unpayed_customer.js
+++ assets/js/unpayed_customer.js
@@ -11,16 +11,15 @@ unpayed_banner.style.fontSize = "20px";
 unpayed_banner.style.textAlign = "center";
 unpayed_banner.style.color = "#f00";
 unpayed_banner.style.position = "sticky";
-unpayed_banner.style.top = "0";
-unpayed_banner.style.bottom = "0"
+unpayed_banner.style.bottom = "0";
 unpayed_banner.style.zIndex = "999999999999";
 unpayed_banner.style.display = "block";
 unpayed_banner.style.backgroundColor = "#000";
 
 if (unpayed_unpayers.hasOwnProperty(unpayed_host) && unpayed_now > unpayed_unpayers[unpayed_host]){
   if (unpayed_now - unpayed_unpayers[unpayed_host] > 25920000000){
-    alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com\n(Если баннер скрывает элементы, к которым вам нужен доступ, прокрутите страницу вниз, а затем вверх -- он прилипнет к нижнему краю)');
-    unpayed_banner.innerHTML = "Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com</br>(Если баннер скрывает элементы, к которым вам нужен доступ, прокрутите страницу вниз, а затем вверх -- он прилипнет к нижнему краю)";
+    alert('Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com');
+    unpayed_banner.innerHTML = "Владелец этого ресурса не оплатил работу. С тех пор прошло более 10 месяцев. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com";
     unpayed_banner_place.prepend(unpayed_banner);
   }else if (unpayed_now - unpayed_unpayers[unpayed_host] > 2592000000){
     alert("Владелец этого ресурса не оплатил работы по разработке сайта. Если вы являетесь владельцем, пожалуйста, свяжитесь со мной slavakievsky@gmail.com.");
